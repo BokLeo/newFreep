cdd044ac35dd2e7542b287e2e0c2576e57504cad
BokLeo <yobad713@naver.com>
2024-10-21 21:09:24 +0900

    241021. 포폴을 위한 레거시 FreeP 재구성 및 DB 재구축

---
+++ README.md
@@ -0,0 +1,5 @@
+# TeamProject FreeP
+Project name : FreeP   
+Concept : 피자 주문 웹   
+KeyPoint : DIY피자 주문   
+배포환경 : Oracle Cloud / Linux OS
---
+++ src/main/java/review/LikedReviewDTO.java
@@ -0,0 +1,9 @@
+package review;
+
+import lombok.Data;
+
+@Data public class LikedReviewDTO {
+	private int like_idx;
+	private int rv_idx;
+	private int m_code;
+}
